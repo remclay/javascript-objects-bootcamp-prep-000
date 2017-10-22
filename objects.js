9f0af5a5ce1f1d46e6a844f10cae5279bc11551f
Remmy Clay <remmy.lauren@gmail.com>
2017-10-22 22:58:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
-var playlist = {Kanye: "Gold Digger"};
+var playlist = { Kanye: "Gold Digger" };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  var updatePlaylist = Object.assign({}, playlist, {Phil Ochs:['Here\'s to the State of Mississippi']});
+  playlist['Phil Ochs'] = 'Here\'s to the State of Mississippi';
 }
 
 function removeFromPlaylist(playlist, artistName){
