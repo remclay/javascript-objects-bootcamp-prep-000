3897df9d268088854d390670778c02b44e7338e5
Remmy Clay <remmy.lauren@gmail.com>
2017-10-22 23:01:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = { Kanye: "Gold Digger" };
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = 'Here\'s to the State of Mississippi'
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
